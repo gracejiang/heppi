31dcfc530623124e241d25c9eb299cfc4df1a196
Grace Jiang <gracjiang@gmail.com>
2020-12-15 02:30:24 -0800

    form works

--- main/views.py
+++ main/views.py
@@ -3,6 +3,17 @@ from main.models import Post
 
 # Create your views here.
 
+# main page
 def main_view(request):
+    # decide between POST & GET request
+
+    # if POST, insert into DB
+    if request.method == 'POST':
+        post = Post.objects.create(
+            title=request.POST['title'],
+            body=request.POST['body'],
+        )
+
+    # if GET, render page
     posts = Post.objects.all()
     return render(request, 'main.html', {'posts': posts})
