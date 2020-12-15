099c3562ba7f05589002ac62841dda87f3c92de3
Grace Jiang <gracjiang@gmail.com>
2020-12-15 03:28:13 -0800

    login, logout, register functionality

--- heppi/urls.py
+++ heppi/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main_view, name='main_view'),
     path('delete', views.delete_view, name='delete_view'),
+    path('accounts/', views.accounts_view, name='accounts_view'),
+    path('login/', views.login_view, name='login_view'),
+    path('signup/', views.signup_view, name='signup_view'),
+    path('logout/', views.logout_view, name='logout_view'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.utils import timezone
 from datetime import datetime
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from main.models import Post
 
 # Create your views here.
@@ -29,4 +30,33 @@ def delete_view(request):
     # post = Post.objects.get(id=request.POST???, request???)
     post = Post.objects.get(id=request.GET['id'])
     post.delete()
+    return redirect('/')
+
+# login/signup page
+def accounts_view(request):
+    return render(request, 'accounts.html', {})
+
+def login_view(request):
+    username, password = request.POST['username'], request.POST['password']
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        return redirect('/')
+    else:
+        return redirect('/accounts?error=LoginError')
+
+def signup_view(request):
+    # print(request.POST['username'], request.POST['password'], request.POST['email'], )
+    user = User.objects.create_user(
+        username=request.POST['username'],
+        password=request.POST['password'],
+        email=request.POST['email'],
+    )
+
+    login(request, user)
+    return redirect('/')
+
+def logout_view(request):
+    logout(request)
     return redirect('/')
